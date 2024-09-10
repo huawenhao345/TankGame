29b15822d9d148caffbf695b38694a0c76709dae
Point Hua <huawenhaohhh@126.com>
2024-09-10 18:33:41 +0800

    style:

--- console.c
+++ console.c
@@ -3,29 +3,6 @@
 #include "player.h"
 #include "console.h"
 
-// TODO:获取屏幕的大小
-
-int initialization(void)
-{
-    // 窗口句柄
-    HANDLE hwnd = GetStdHandle(STD_OUTPUT_HANDLE);
-    HANDLE hwnd2 = GetConsoleWindow();
-
-    // 控制窗口大小
-    char chCmd[32];
-    sprintf(chCmd, "mode con cols=%d lines=%d", MAP_LENGTH * CHARWHITH, MAP_WIDTH); // 这个以字符大小为单位
-    system(chCmd);
-
-    // 隐藏光标
-    CONSOLE_CURSOR_INFO cursor_info = {1, 0}; // CONSOLE_CURSOR_INFO结构体包含控制台光标的信息,DWORD dwSize光标百分比厚度（1~100）和BOOL bVisible光标是否可见
-    SetConsoleCursorInfo(hwnd, &cursor_info); // SetConsoleCursorInfo用来设置指定的控制台光标的大小和可见性。
-
-    SetConsoleTitle("TankGame");        // 设置窗口标题
-    MoveWindow2(hwnd2, 100, 100, TRUE); // TODO:搞清楚为什么MoveWindow2(hwnd, 500, 500, TRUE);不起作用
-
-    return 0;
-}
-
 RECT GetWindowRect2(HWND hwnd)
 { // 换个名称即可
     RECT rt;
@@ -45,7 +22,7 @@ LONG GetWindowHeight(HWND hwnd)
     return (rect.bottom - rect.top);
 }
 
-WINBOOL MoveWindow2(HWND hwnd, int X, int Y, BOOL bRepaint) // 2021/07/23 修改此处：删除C语言不支持的bool和默认参数
+WINBOOL MoveWindow2(HWND hwnd, int X, int Y, BOOL bRepaint)
 {
     LONG w, h;
     w = GetWindowWidth(hwnd);
@@ -71,4 +48,29 @@ WINBOOL WindowShake(HWND hwnd)
         }
     }
     return MoveWindow2(hwnd, rt.left, rt.top, TRUE);
+}
+
+int initialization(void)
+{
+    // 窗口句柄
+    HANDLE hwnd = GetStdHandle(STD_OUTPUT_HANDLE);
+    HANDLE hwnd2 = GetConsoleWindow();
+
+    // 控制窗口大小
+    char chCmd[32];
+    sprintf(chCmd, "mode con cols=%d lines=%d", MAP_LENGTH * CHARWHITH, MAP_WIDTH); // 这个以字符大小为单位
+    system(chCmd);
+
+    // 隐藏光标
+    CONSOLE_CURSOR_INFO cursor_info = {1, 0};
+    SetConsoleCursorInfo(hwnd, &cursor_info);
+
+    SetConsoleTitle("TankGame"); // 设置窗口标题
+    MoveWindow2(hwnd2,
+                GetSystemMetrics(SM_CXFULLSCREEN) / 2 - GetWindowWidth(hwnd2) / 2,
+                GetSystemMetrics(SM_CYFULLSCREEN) / 2 - GetWindowHeight(hwnd2) / 2,
+                TRUE); // 定位到屏幕中间
+    // TODO:搞清楚为什么MoveWindow2用hwnd不起作用
+
+    return 0;
 }
--- main.c
+++ main.c
@@ -3,36 +3,43 @@
 #include "map.h"
 #include "player.h"
 #include "console.h"
+#include "AI.h"
 
 Block GameMap[MAP_WIDTH][MAP_LENGTH] = {};
 
 // TODO:控制台控制
 // TODO:玩家控制
-// TODO:敌人a
+// TODO:敌人ai
 
 Point PlayerPoint;
+Point AIPoint[AIMAX];
 
 int main(void)
 {
+RE:
 #ifndef TEST
     initialization();
 #endif
 
     RcursiveMapGenerate(GameMap);
 
-    PlayerPoint = AddPlayer(GameMap);
+    PlayerPoint = AddPlayer(GameMap, player);
+    AIPoint[0] = AddPlayer(GameMap, enemy);
     PrintMap(GameMap);
 #ifdef TEST
     PlayerMove(GameMap, &PlayerPoint, UP);
     PlayerMove(GameMap, &PlayerPoint, DOWN);
     PlayerMove(GameMap, &PlayerPoint, LEFT);
     PlayerMove(GameMap, &PlayerPoint, RIGHT);
-#endif
-    PrintMap(GameMap);
 
+    PrintMap(GameMap);
+#endif
+
+#ifndef TEST
     HANDLE hwnd2 = GetConsoleWindow();
     WindowShake(hwnd2);
-    while (1)
+
+    while (TRUE)
     {
 
         if (KEY_DOWN_FOREGROUND(hwnd2, VK_UP))
@@ -55,9 +62,13 @@ int main(void)
             PlayerMove(GameMap, &PlayerPoint, RIGHT);
             PrintMap(GameMap);
         }
+        if (KEY_DOWN_FOREGROUND(hwnd2, 'R')) // 小写的r不行
+        {
+            goto RE;
+        }
         Sleep(10);
     }
     getch();
-
+#endif
     return 0;
 }
