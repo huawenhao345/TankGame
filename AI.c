dc92ff43163366aec69d094f4aa51c8037331dd8
Point Hua <huawenhaohhh@126.com>
2024-09-11 23:25:40 +0800

    update

--- AI.c
+++ AI.c
@@ -1,73 +1,39 @@
 #include "AI.h"
 #include "map.h"
 #include "player.h"
+#include <stdlib.h>
 
-int AIMove(Block Map[MAP_WIDTH][MAP_LENGTH], Point *PlayerPoint, Point *AIPoint) // 使用自动寻路A*算法
+node *CreateNode(Point StartPoint)
+{
+    node *n = (node *)malloc(sizeof(node));
+    n->point = StartPoint;
+    n->next = NULL;
+    return n;
+}
+
+int FreeList(node *list)
+{
+    while (list != NULL)
+    {
+        node *p = list;
+        list = list->next;
+        free(p);
+    }
+    return 0;
+}
+
+int AddList(node *lastnode, Point point)
 {
 }
 
-// use A* algorithm to find the path
-int AStar(Block Map[MAP_WIDTH][MAP_LENGTH], Point *PlayerPoint, Point *AIPoint)
+int RemoveNode(node *list, Point point)
 {
-    // init
-    int OpenList[MAP_WIDTH][MAP_LENGTH] = {0};  // open list
-    int CloseList[MAP_WIDTH][MAP_LENGTH] = {0}; // close list
-    int Path[MAP_WIDTH][MAP_LENGTH] = {0};      // path
-    int F[MAP_WIDTH][MAP_LENGTH] = {0};         // F
-    int G[MAP_WIDTH][MAP_LENGTH] = {0};         // G
-    int H[MAP_WIDTH][MAP_LENGTH] = {0};         // H
-    int x, y;
-    int x1, y1;
-    int x2, y2;
-    int x0, y0;
-    int i;
-    int j;
-    int m;
-    int n;
-    int k;
-    int t;
-    int temp;
-    int way;
-    int flag;
-    int step = 0;
-    int dir[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // right, down, left, up
-    int startx = AIPoint->x;
-    int starty = AIPoint->y;
-    int endx = PlayerPoint->x;
-    int endy = PlayerPoint->y;
+}
 
-    // init
-    for (i = 0; i < MAP_WIDTH; i++)
-    {
-        for (j = 0; j < MAP_LENGTH; j++)
-        {
-            if (Map[i][j] == wall)
-            {
-                CloseList[i][j] = 1;
-            }
-        }
-    }
+node *SearchNode()
+{
+}
 
-    // start point
-    x1 = startx;
-    y1 = starty;
-    // end point
-    x2 = endx;
-    y2 = endy;
-
-    // init
-    for (i = 0; i < MAP_WIDTH; i++)
-    {
-        for (j = 0; j < MAP_LENGTH; j++)
-        {
-            G[i][j] = 0;
-            H[i][j] = abs(x2 - i) + abs(y2 - j);
-            F[i][j] = G[i][j] + H[i][j];
-        }
-    }
-
-    // init
-    OpenList[x1][y1] = 1;
-    flag = 0;
-    way = 0;
+int AIMove(Block Map[MAP_WIDTH][MAP_LENGTH], Point PlayerPoint, Point AIPoint) // 使用自动寻路A*算法
+{
 }
--- head/AI.h
+++ head/AI.h
@@ -1,6 +1,13 @@
 #ifndef _AI_H
 #define _AI_H 1
 
+#include "map.h"
 #define AIMAX 5
+
+typedef struct
+{
+    Point point;
+    node *next;
+} node,*pnote;
 
 #endif
