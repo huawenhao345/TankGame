a6e91382ac1f160c6a107b311fcbd8fcda842b1a
Point Hua <huawenhaohhh@126.com>
2024-09-16 22:51:35 +0800

    feat: 实现A*寻路

--- AI.c
+++ AI.c
@@ -80,4 +80,140 @@ node *SearchNode(node *list, Point cpoint)
 
 int AIMove(Block Map[MAP_WIDTH][MAP_LENGTH], Point PlayerPoint, Point AIPoint) // 使用自动寻路A*算法
 {
+    // 1.初始化open表和close表
+    node *open = CreateNode(AIPoint);
+    node *close = NULL;
+    node *current = NULL;
+    node *end = open;
+    Point tempPoint;
+    int i, j;
+    int G, H, F;
+    int flag = 0;
+    while (open != NULL)
+    {
+        current = open;
+        end = open;
+        while (current != NULL)
+        {
+            if (current->point.x == PlayerPoint.x && current->point.y == PlayerPoint.y)
+            {
+                flag = 1;
+                break;
+            }
+            // 上
+            tempPoint.x = current->point.x;
+            tempPoint.y = current->point.y - 1;
+            if (tempPoint.y >= 0 && Map[tempPoint.x][tempPoint.y] != wall && SearchNode(open, tempPoint) == NULL && SearchNode(close, tempPoint) == NULL)
+            {
+                end = AddList(end, tempPoint);
+            }
+            // 下
+            tempPoint.x = current->point.x;
+            tempPoint.y = current->point.y + 1;
+            if (tempPoint.y < MAP_LENGTH && Map[tempPoint.x][tempPoint.y] != wall && SearchNode(open, tempPoint) == NULL && SearchNode(close, tempPoint) == NULL)
+            {
+                end = AddList(end, tempPoint);
+            }
+            // 左
+            tempPoint.x = current->point.x - 1;
+            tempPoint.y = current->point.y;
+            if (tempPoint.x >= 0 && Map[tempPoint.x][tempPoint.y] != wall && SearchNode(open, tempPoint) == NULL && SearchNode(close, tempPoint) == NULL)
+            {
+                end = AddList(end, tempPoint);
+            }
+            // 右
+            tempPoint.x = current->point.x + 1;
+            tempPoint.y = current->point.y;
+            if (tempPoint.x < MAP_WIDTH && Map[tempPoint.x][tempPoint.y] != wall && SearchNode(open, tempPoint) == NULL && SearchNode(close, tempPoint) == NULL)
+            {
+                end = AddList(end, tempPoint);
+            }
+            current = current->next;
+        }
+        if (flag == 1)
+        {
+            break;
+        }
+        // 将当前节点加入close表
+        close = AddList(close, current->point);
+        // 将当前节点从open表中删除
+        RemoveNode(open, current->point);
+    }
+    if (flag == 0)
+    {
+        FreeList(open);
+        FreeList(close);
+        return -1;
+    }
+    // 找到路径
+    node *path = CreateNode(PlayerPoint);
+    while (current->point.x != AIPoint.x || current->point.y != AIPoint.y)
+    {
+        // 上
+        tempPoint.x = current->point.x;
+        tempPoint.y = current->point.y - 1;
+        if (tempPoint.y >= 0 && SearchNode(close, tempPoint) != NULL)
+        {
+            path = AddList(path, tempPoint);
+            current = SearchNode(close, tempPoint);
+            continue;
+        }
+        // 下
+        tempPoint.x = current->point.x;
+        tempPoint.y = current->point.y + 1;
+        if (tempPoint.y < MAP_LENGTH && SearchNode(close, tempPoint) != NULL)
+        {
+            path = AddList(path, tempPoint);
+            current = SearchNode(close, tempPoint);
+            continue;
+        }
+        // 左
+        tempPoint.x = current->point.x - 1;
+        tempPoint.y = current->point.y;
+        if (tempPoint.x >= 0 && SearchNode(close, tempPoint) != NULL)
+        {
+            path = AddList(path, tempPoint);
+            current = SearchNode(close, tempPoint);
+            continue;
+        }
+        // 右
+        tempPoint.x = current->point.x + 1;
+        tempPoint.y = current->point.y;
+        if (tempPoint.x < MAP_WIDTH && SearchNode(close, tempPoint) != NULL)
+        {
+            path = AddList(path, tempPoint);
+            current = SearchNode(close, tempPoint);
+            continue;
+        }
+    }
+    // 移动
+    while (path->next != NULL)
+    {
+        if (PlayerPoint.x == path->next->point.x && PlayerPoint.y == path->next->point.y)
+        {
+            break;
+        }
+        if (PlayerPoint.x == path->next->point.x && PlayerPoint.y == path->next->point.y - 1)
+        {
+            PlayerMove(Map, &PlayerPoint, UP);
+        }
+        else if (PlayerPoint.x == path->next->point.x && PlayerPoint.y == path->next->point.y + 1)
+        {
+            PlayerMove(Map, &PlayerPoint, DOWN);
+        }
+        else if (PlayerPoint.x == path->next->point.x - 1 && PlayerPoint.y == path->next->point.y)
+        {
+            PlayerMove(Map, &PlayerPoint, LEFT);
+        }
+        else if (PlayerPoint.x == path->next->point.x + 1 && PlayerPoint.y == path->next->point.y)
+        {
+            PlayerMove(Map, &PlayerPoint, RIGHT);
+        }
+        GoToxy(PlayerPoint.x * CHARWHITH, PlayerPoint.y);
+        path = path->next;
+    }
+    FreeList(open);
+    FreeList(close);
+    FreeList(path);
+    return 0;
 }
